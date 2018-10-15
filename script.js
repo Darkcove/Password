a23e9e5051d476218d7def353490744e82f89fa3
Darkcove <shozdott@gmail.com>
2018-10-15 00:29:42 -0700

    Password Generator

---
+++ script.js
@@ -0,0 +1,33 @@
+function generate() {
+  let complexity = document.getElementById('slider').value;
+  let values =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&()*+,-./:;<=>?@[]^_{|}~';
+  let password = '';
+
+  for (var i = 0; i <= complexity; i++) {
+    password =
+      password +
+      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+  document.getElementById('display').value = password;
+  document.getElementById('lastNums').innerHTML += password + '<br />';
+}
+
+document.getElementById('Length').innerHTML = 'Length: 25';
+
+document.getElementById('slider').oninput = function() {
+  if (document.getElementById('slider').value > 0) {
+    document.getElementById('Length').innerHTML =
+      'Length:' + document.getElementById('slider').value;
+  } else {
+    document.getElementById('Length').innerHTML = 'Length: 1';
+  }
+};
+
+function copyPassword() {
+  document.getElementById('display').select();
+
+  document.execCommand('Copy');
+
+  alert('password copied to clipboard');
+}
